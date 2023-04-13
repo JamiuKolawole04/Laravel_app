deeda62e267b977c9bc3eb3f378513b17a276990
JamiuKolawole04 <Jakko4real@gmail.com>
2023-04-13 21:03:37 +0100

    chore: adding resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/**
+ * GET - Request a resource
+ * POST - Create a resource
+ * PUT - update a resource
+ * PATCH - Modify a resource
+ * DELETE - Delete a resource
+ * OPTIONS - Ask the server which verb is allowed
+ * 
+ */
 
 Route::get('/', function () {
     Debugbar::addMessage("INFO!");
@@ -34,3 +45,6 @@ Route::get('/', function () {
 
 // Route::get("/blog", [PostsController::class, "index"]);
 Route::resource("blog", PostsController::class);
+
+// caaling theinvol with a route
+Route::get("/involke", HomeController::class);
