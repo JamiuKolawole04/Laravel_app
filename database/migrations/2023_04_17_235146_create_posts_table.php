1e26114d92d02f028826ac3b8caa6e4ccbd8f8ab
JamiuKolawole04 <Jakko4real@gmail.com>
2023-04-18 01:13:27 +0100

    chore: migrating db

--- database/migrations/2023_04_17_235146_create_posts_table.php
+++ database/migrations/2023_04_17_235146_create_posts_table.php
@@ -14,7 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
+            // default primary key created by Laravel
+            // $table->id()->primary();
             $table->id();
+            // string === 255 character limit
+            $table->string("title")->unique();
+            // text === 30, 0000 character limit
+            $table->text("excerpt")->nullable();
+            $table->text("body");
+            $table->integer("min_to_read")->default(1);
+            $table->string("image_path");
+            $table->boolean("is_published");
             $table->timestamps();
         });
     }
