250ca4b109cbcac4d065cb25092d5120b1f656b4
JamiuKolawole04 <Jakko4real@gmail.com>
2023-04-26 09:16:51 +0100

    chore: making change

--- routes/web.php
+++ routes/web.php
@@ -27,80 +27,80 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/', function () {
-    Debugbar::addMessage("INFO!");
+// Route::get('/', function () {
+//     Debugbar::addMessage("INFO!");
 
-    // try {
+//     // try {
 
-    //     throw new Exception("Try message");
-    // } catch(Exception $e) {
-    //     Debugbar::addException($e);
-    // }
+//     //     throw new Exception("Try message");
+//     // } catch(Exception $e) {
+//     //     Debugbar::addException($e);
+//     // }
 
-    $name = "Code with Bhary";
+//     $name = "Code with Bhary";
     
-    return view('welcome', [
-        "name" => $name,
-    ]); 
-});
+//     return view('welcome', [
+//         "name" => $name,
+//     ]); 
+// });
 
 // GET Requests
 // Route::resource("blog", PostsController::class);
-Route::get("/blog", [PostsController::class, "index"]);
-Route::get("/index", [PostsController::class, "default"]);
-Route::get("/indexTwo", [PostsController::class, "defaultTwo"]);
-Route::get("/blog2", [PostsController::class, "indexTwo"])->name("blog2");
-Route::get("/article/{id?}", [PostsController::class, "show"]);
+// Route::get("/blog", [PostsController::class, "index"]);
+// Route::get("/index", [PostsController::class, "default"]);
+// Route::get("/indexTwo", [PostsController::class, "defaultTwo"]);
+// Route::get("/blog2", [PostsController::class, "indexTwo"])->name("blog2");
+// Route::get("/article/{id?}", [PostsController::class, "show"]);
 // adding regular expression only to route id and makng sure it's an integer
-Route::get("/blog/{id}", [PostsController::class, "show"])->where("id", "[0-9]+")->name("blogWithId");
-Route::get("/create", [PostsController::class, "create"]);
-Route::post("/store", [PostsController::class, "store"]);
-Route::get("/edit/{id}", [PostsController::class, "edit"])->name("edit")->whereNumber("id");
-Route::patch("/update/{id}", [PostsController::class, "update"])->name("update");
-Route::delete("/delete/{id}", [PostsController::class, "destroy"])->name("delete");
+// Route::get("/blog/{id}", [PostsController::class, "show"])->where("id", "[0-9]+")->name("blogWithId");
+// Route::get("/create", [PostsController::class, "create"]);
+// Route::post("/store", [PostsController::class, "store"]);
+// Route::get("/edit/{id}", [PostsController::class, "edit"])->name("edit")->whereNumber("id");
+// Route::patch("/update/{id}", [PostsController::class, "update"])->name("update");
+// Route::delete("/delete/{id}", [PostsController::class, "destroy"])->name("delete");
 
 
 
 
 // checking number with whereNumber
-Route::get("/blog/number/{id}", [PostsController::class, "show"])->whereNumber("id");
+// Route::get("/blog/number/{id}", [PostsController::class, "show"])->whereNumber("id");
 // adding regular expression only to route id and makng sure it's a string or they are strings
-Route::get("/blogTwo/{name}", [PostsController::class, "show"])->where("name", "[A-Za-z]+");
+// Route::get("/blogTwo/{name}", [PostsController::class, "show"])->where("name", "[A-Za-z]+");
 
 // checking alphabet with where alpha
-Route::get("/blogTwo/{name}", [PostsController::class, "show"])->whereAlpha("name");
+// Route::get("/blogTwo/{name}", [PostsController::class, "show"])->whereAlpha("name");
 
 // check whether id is number and name is alphabet with where statement
-Route::get("/blogThree/{id}/{name}", [PostsController::class, "show"])
-    ->where([
-        "id" => "[0-9]+",
-        "name" => "[A-Za-z]+"
-    ]);
+// Route::get("/blogThree/{id}/{name}", [PostsController::class, "show"])
+//     ->where([
+//         "id" => "[0-9]+",
+//         "name" => "[A-Za-z]+"
+//     ]);
 
 // check whether id is number and name is alphabet with where statement
 
-Route::get("/blogThree/{id}/{name}", [PostsController::class, "show"])
-   ->whereNumber("id")
-   ->whereAlpha("name");
+// Route::get("/blogThree/{id}/{name}", [PostsController::class, "show"])
+//    ->whereNumber("id")
+//    ->whereAlpha("name");
 
     
 
 // POST Requests
-Route::post("/blog/create", [PostsController::class, "create"]);
-Route::post("/blog/{id}", [PostsController::class, "store"]);
+// Route::post("/blog/create", [PostsController::class, "create"]);
+// Route::post("/blog/{id}", [PostsController::class, "store"]);
 
 // PUT or PATCHR equests
-Route::put("/blog/{id}", [PostsController::class, "edit"]);
-Route::patch("/blog/{id}", [PostsController::class, "update"]);
+// Route::put("/blog/{id}", [PostsController::class, "edit"]);
+// Route::patch("/blog/{id}", [PostsController::class, "update"]);
 
 // DELETE Requests
-Route::delete("/blog/{id}", [PostsController::class, "destroy"]);
+// Route::delete("/blog/{id}", [PostsController::class, "destroy"]);
 
 // prefixed route
-Route::prefix("/second")->group(function () {
-     Route::get("/", [PostsController::class, "indexTwo"]);
-    Route::get("/user", [PostsController::class, "user"]);
-});
+// Route::prefix("/second")->group(function () {
+//      Route::get("/", [PostsController::class, "indexTwo"]);
+//     Route::get("/user", [PostsController::class, "user"]);
+// });
 
 
 // MULTIPLE VERBS
@@ -110,12 +110,14 @@ Route::prefix("/second")->group(function () {
 // Route::any("/blog", [PostsController::class, "index"]);
 
 // Return view without needing controllers
-Route::view( "/view ", "blog.index", ["name" => "code with dhary"]);
+// Route::view( "/view ", "blog.index", ["name" => "code with dhary"]);
 
 
 
 // caaling theinvolk with a route
-Route::get("/involke", HomeController::class);
+// Route::get("/involke", HomeController::class);
 
 // fallback route
 Route::fallback(FallbackController::class);
+
+Route::resource("blog", PostsController::class);
